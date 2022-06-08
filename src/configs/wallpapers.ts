85f2492f81bbe2ae11b1d162306fcd8c80e49e1b
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-06-08 20:22:26 +0530

    Update wallpapers.ts

--- src/configs/wallpapers.ts
+++ src/configs/wallpapers.ts
@@ -1,8 +1,8 @@
 import type { WallpaperData } from "~/types";
 
 const wallpapers: WallpaperData = {
-  day: "img/ui/wallpaper-day.webp",
-  night: "img/ui/wallpaper-night.webp"
+  day: "img/ui/wallpaper-day.jpg",
+  night: "img/ui/wallpaper-night.jpg"
 };
 
 export default wallpapers;
